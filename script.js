a3db8dfe055dfc60a1e71677e9854ba9f7cea47e
KANCHANA-88 <105222558+KANCHANA-88@users.noreply.github.com>
2023-10-02 21:44:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,3 +20,44 @@ function updateCart() {
 
     totalElement.innerText = total;
 }
+
+
+
+function showLogin() {
+    document.getElementById('login-section').classList.remove('hidden');
+    document.getElementById('registration-section').classList.add('hidden');
+    document.getElementById('main-section').classList.add('hidden');
+}
+
+function showRegistration() {
+    document.getElementById('login-section').classList.add('hidden');
+    document.getElementById('registration-section').classList.remove('hidden');
+    document.getElementById('main-section').classList.add('hidden');
+}
+
+function login() {
+    // Implement login logic (e.g., check credentials)
+    // If successful, show main section and hide login and registration sections
+    document.getElementById('login-section').classList.add('hidden');
+    document.getElementById('registration-section').classList.add('hidden');
+    document.getElementById('main-section').classList.remove('hidden');
+
+    return false; // Prevent form submission
+}
+
+function register() {
+    // Implement registration logic (e.g., save user to a mock database)
+    // If successful, show main section and hide login and registration sections
+    document.getElementById('login-section').classList.add('hidden');
+    document.getElementById('registration-section').classList.add('hidden');
+    document.getElementById('main-section').classList.remove('hidden');
+
+    return false; // Prevent form submission
+}
+
+// Add product and cart logic similar to the previous example
+
+function checkout() {
+    alert('Thank you for your purchase!');
+}
+
